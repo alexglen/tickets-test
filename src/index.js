84ee6916f32a3ae1f0f1dff87ab807f4df984010
Александр <alexsmoleev1990@gmail.com>
2024-04-17 10:04:36 +0300

    init commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h1>Hola!</h1>
+  </React.StrictMode>
+);
